7dea62fcd5c638066e79d1988a12eaf5927de916
martina <martinrodriguez@itba.edu.ar>
2022-03-09 12:26:57 -0300

    initial setup
    Co-authored-by: anita <atarantino@itba.edu.ar>

---
+++ TP1/puzzle_maker.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def create_puzzle():
+
+    random_matrix_array = np.random.rand(3)
+    print(random_matrix_array)
--- TP1/puzzle_solver.py
+++ TP1/puzzle_solver.py
@@ -1,17 +1,27 @@
 import sys
 from typing import Collection
 
+from TP1.puzzle_maker import create_puzzle
 from TP1.state import State
 
 
-def main(config_file: str):
-    states:Collection[State] = puzzle_solver()
+# def main(config_file: str):
+#     states:Collection[State] = puzzle_solver()
 
-def puzzle_solver()->Collection[State]:
-    # TODO
+def main():
+    puzzle_solver()
+
+# def puzzle_solver()->Collection[State]:
+#     # TODO
+def puzzle_solver():
+    create_puzzle()
 
 if __name__ == "__main__":
     argv = sys.argv
     try:
-        main(config_file)
-    print('hola')
+        # main(config_file)
+        main()
+
+    except FileNotFoundError as e:
+        print(f'Configuration file {e.filename} not found')
+
