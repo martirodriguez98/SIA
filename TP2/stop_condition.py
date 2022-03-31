9f685581cef710740ce0bb390eea4ad81bc45511
martina <martinrodriguez@itba.edu.ar>
2022-03-31 11:54:40 -0300

    fix stop condition

--- TP2/stop_condition.py
+++ TP2/stop_condition.py
@@ -67,19 +67,19 @@ def get_stop_condition(params: Param) -> StopCondition:
 
 def stop_condition_met(stop_condition: StopCondition, gen: Generation, bag: Bag) -> bool:
     if gen.gen_count > stop_condition.gen_count:
-        if time.perf_counter() > stop_condition.time:
-            if valid_solution(gen,bag):
-                print('Time is over. A solution was found.')
-            else:
-                print('Time is over. A solution could not be found.')
-            return True
-
         if unchanged_generations(stop_condition,gen,bag):
             print(f'{stop_condition.percentage}% of the population has remain the same for over {stop_condition.unchanged_gens} generations.')
             return True
 
         if fitness_gen_count(stop_condition,gen,bag):
             print(f'The best fitness has been the same over {stop_condition.fitness_gen_count} generations')
+            return True
+
+        if time.perf_counter() > stop_condition.time:
+            if valid_solution(gen,bag):
+                print('Time is over. A solution was found.')
+            else:
+                print('Time is over. A solution could not be found.')
             return True
 
     return False
