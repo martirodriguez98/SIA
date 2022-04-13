1b084b349d5a45938b1a30e8de0494c99113982a
Micky <mconca@itba.edu.ar>
2022-04-13 20:43:44 -0300

    progress in algorithm

--- TP3/neural_networks.py
+++ TP3/neural_networks.py
@@ -3,6 +3,10 @@ from abc import ABC
 
 import numpy as np
 
+# chequear si la tenemos que definir nosotras o la pasan como parametro
+COTA = 10
+n = 0.1
+
 
 class NeuralNetworkConfig:
     def __init__(self):
@@ -17,22 +21,26 @@ class NeuralNetwork(ABC):
         self.config_neural: NeuralNetworkConfig = config_neural
 
     def train(self, x: np.ndarray, y: np.ndarray):
+        p: int = len(y)
         i: int = 0
         w = np.zeros(self.config_neural.x_count + 1)
         error: int = 1
-        error_min = len(y) * 2
-        while error > 0:
+        error_min = p * 2
+        while error > 0 and i < COTA:
             i_x = random.randint(1, len(y))
             h = x[i_x].w
-            # o = signo(h)
-            # delta_w = n * (y[i_x] - o).x[i_x]
-            # w = w + delta_w
-            # error=calcular error
+            o = np.sign(h)
+            delta_w = n * (y[i_x] - o).x[i_x]
+            w = w + delta_w
+            error = self.calcular_error(x, y, w, p)
             if error < error_min:
                 error_min = error
                 w_min = w
             i = i + 1
-        
+
+    def calcular_error(self, x: np.ndarray, y: np.ndarray, w: np.array, p: int):
+        return 1
+
 
 class SingleNeuralNetwork(NeuralNetwork, ABC):
 
