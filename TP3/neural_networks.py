a32a57b2603b7b1bdf95f4fa371e210954001222
Anita <atarantino@itba.edu.ar>
2022-04-19 20:12:31 -0300

    Auto stash before merge of "main" and "origin/main"
    algorithm fixes

--- TP3/neural_networks.py
+++ TP3/neural_networks.py
@@ -4,8 +4,8 @@ from plot import plot
 import numpy as np
 
 # chequear si la tenemos que definir nosotras o la pasan como parametro
-COTA = 10
-n = 0.1
+COTA = 10000
+n = 0.01
 
 
 class NeuralNetworkConfig:
@@ -19,32 +19,41 @@ class NeuralNetworkConfig:
 class NeuralNetwork(ABC):
     def __init__(self, config_neural: NeuralNetworkConfig):
         self.config_neural: NeuralNetworkConfig = config_neural
+        self.plot = {"x": [], "y": []}
 
     def train(self, x: np.ndarray, y: np.ndarray):
         p: int = len(y)
         i: int = 0
         w = np.zeros(self.config_neural.x_count)
+        w_min = w
         all_w: list = []
         all_w.append(w)
         error: int = 1
         error_min = p * 2
         while error > 0 and i < COTA:
-            i_x = random.randint(1, len(y)-1)
-            h = np.dot(x[i_x],w)
-            o = np.sign(h)
-            delta_w = np.dot((n * (y[i_x] - o))[0],x[i_x])
-            # delta_w = n * (y[i_x] - o).x[i_x]
+            print(x)
+            print(w)
+            i_x = random.randint(1, p - 1)
+            h = np.dot(x[i_x], w)
+            o = np.copysign(1, h)
+            delta_w = n * (y[i_x] - o) * x[i_x]
             w = w + delta_w
             all_w.append(w)
-            error = self.calcular_error(x, y, w, p)
+            error = self.calculate_error(x, y, w, p)
             if error < error_min:
                 error_min = error
                 w_min = w
             i = i + 1
-        plot(all_w)
-        
-    def calcular_error(self, x: np.ndarray, y: np.ndarray, w: np.array, p: int):
-        return 1
+        print(w_min)
+        self.plot["x"].append(np.arange(-1, 2))
+        self.plot["y"].append((-w_min[1] / w_min[2]) * np.arange(-1, 2) - w_min[0] / w_min[2])
+        plot(self.plot)
+
+    def calculate_error(self, x: np.ndarray, y: np.ndarray, w: np.array, p: int):
+        o = []
+        for i in range(p):
+            o.append(abs(y[i] - np.copysign(1, np.dot(x[i], w))))
+        return sum(o)
 
 
 class SingleNeuralNetwork(NeuralNetwork, ABC):
@@ -53,7 +62,7 @@ class SingleNeuralNetwork(NeuralNetwork, ABC):
         config.single = "en single"
         super().__init__(config)
 
-    def train(self,  x: np.ndarray, y: np.ndarray):
+    def train(self, x: np.ndarray, y: np.ndarray):
         super().train(x, y)
 
 
--- TP3/plot.py
+++ TP3/plot.py
@@ -2,24 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot(all_w: list):
-    x = [-1, 1, -1, 1]
-    y = [1, -1, -1, 1]
-    print(all_w)
-    # x.append(w[1])
-    # y.append(w[2])
-    x_w = []
-    y_w = []
-    for e in all_w:
-        x_w.append(e[1])
-        y_w.append(e[2])
+def plot(plot_info: dict):
+    x = np.array([[1, -1, 1], [1, 1, -1], [1, -1, -1], [1, 1, 1]])
+    y = np.array([-1, -1, -1, 1])
 
-    plt.axhline(0,color="black")
-    plt.axvline(0,color="black")
-    plt.scatter(x,y)
-    plt.scatter(x_w,y_w)
-    plt.xlabel("X")
-    plt.ylabel("Y")
+    fig, ax = plt.subplots(figsize=(10, 8), layout="constrained")
 
-    plt.title("Points")
+    ax.set_xlabel("x", fontsize=15)
+    ax.set_ylabel("y", fontsize=15)
+    plt.scatter(x, y)
+    plt.xlim(0, 5)
+    plt.ylim(0, 5)
+    ax.locator_params("y", nbins=10)
+    ax.locator_params("x", nbins=10)
+    ax.plot(x, plot_info["x"], label="x", color="r")
+    ax.plot(x, plot_info["y"], label="y", color="b")
+    ax.legend(prop={"size": 20})
     plt.show()
+
+    # def plot(all_w: list):
+#     x = [-1, 1, -1, 1]
+#     y = [1, -1, -1, 1]
+#     print(all_w)
+#     # x.append(w[1])
+#     # y.append(w[2])
+#     x_w = []
+#     y_w = []
+#     for e in all_w:
+#         x_w.append(e[1])
+#         y_w.append(e[2])
+#
+#     plt.axhline(0,color="black")
+#     plt.axvline(0,color="black")
+#     plt.scatter(x,y)
+#     plt.scatter(x_w,y_w)
+#     plt.xlabel("X")
+#     plt.ylabel("Y")
+#
+#     plt.title("Points")
+#     plt.show()
