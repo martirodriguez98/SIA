ee504aa5bf21c178a87f4778b58f48e59cf1ebc7
martina <martinrodriguez@itba.edu.ar>
2022-06-14 19:06:45 -0300

    parse input
    Co-authored-by: anita <atarantino@itba.edu.ar>
    Co-authored-by: micky <mconca@itba.edu.ar>

--- TP5/utils.py
+++ TP5/utils.py
@@ -1,27 +1,13 @@
 import numpy as np
 
 def transform_input(values: np.ndarray) -> np.ndarray:
-    new_values = np.empty((np.size(values, 0), np.size(values, 1) * 5))
-    print(f'size(values,0) = {np.size(values,0)}')
-    print(f'size(values,1) = {np.size(values,1)}')
-#
-#     # for i in range(np.size(values,0)):
-#     #     bits = ""
-#     #     for j in range(np.size(values,1)):
-#     #         binary_value = str(str(bin(values[i][j]))[2:].zfill(5))
-#     #         bits += binary_value
-#     #         print(binary_value)
-#     #         print(f'bits: {bits}')
-#     #     np.append(new_values[i], int(bits))
-#     #     print(new_values[i])
-    print(np.array("001"))
+    new_values = np.empty((np.size(values, 0), np.size(values, 1) * 5)).astype(int)
     for i in range(np.size(values,0)):
+        total_bits: list = []
         for j in range(np.size(values,1)):
             binary_value = bin(values[i][j])[2:].zfill(5)
-            binary_array: np.array = np.array([binary_value])
-            for k in range(np.size(binary_array)):
-                np.append(new_values[i], binary_array[k])
-        # print(new_values[i])
+            total_bits.append(list(binary_value))
+        new_values[i] = np.array(total_bits).flatten()
     return new_values
 
 
